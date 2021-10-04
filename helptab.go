6ebc83d8dc720218c225e67505f43dec1bb73f29
Amaury Bodet <amaury@mailbox.org>
2021-10-04 09:16:33 +0200

    Add download option for universal adb drivers

--- helptab.go
+++ helptab.go
@@ -11,17 +11,26 @@ import(
 )
 
 func helptab() fyne.CanvasObject {
-	tryfirst := widget.NewLabel("If your device is not detected, restart your computer and try another USB cable, port or hub.\nIf you are on Windows, try to install the official drivers for your device.")
+	// Left side
+	tryfirst := widget.NewLabel("If your device is not detected, restart your computer and try another USB cable, port or hub.\nIf you are on Windows, try to install drivers for your device.")
 	tryfirst.Wrapping = fyne.TextWrapWord
 
-	href := "https://developer.android.com/studio/run/oem-usb#Drivers"
-	u, err := url.Parse(href)
+	href1 := "https://developer.android.com/studio/run/oem-usb#Drivers"
+	u1, err := url.Parse(href1)
 	if err != nil {
-		logger.LogError("unable to parse " + href + " as URL:", err)
+		logger.LogError("unable to parse " + href1 + " as URL:", err)
 	}
-	link_to_drivers := widget.NewHyperlinkWithStyle("Download drivers", u, fyne.TextAlignCenter, fyne.TextStyle{})
+	link_to_official_drivers := widget.NewHyperlinkWithStyle("Official drivers", u1, fyne.TextAlignCenter, fyne.TextStyle{})
 
-	leftside := container.NewVBox(tryfirst, widget.NewLabel(""), link_to_drivers)
+	href2 := "https://stuff.free-droid.com/universaladbdriver_v6.0.zip"
+	u2, err := url.Parse(href2)
+	if err != nil {
+		logger.LogError("unable to parse " + href2 + " as URL:", err)
+	}
+	link_to_universal_drivers := widget.NewHyperlinkWithStyle("Universal drivers", u2, fyne.TextAlignCenter, fyne.TextStyle{})
+
+
+	leftside := container.NewVBox(tryfirst, widget.NewLabel(""), container.NewCenter(container.NewHBox(link_to_official_drivers, link_to_universal_drivers)))
 	leftcard := widget.NewCard("", "", leftside)
 
 	rightside := container.NewVBox()
