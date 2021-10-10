54845dd64af14465d5820c185f93fb0c3920da54
Amaury Bodet <amaury@mailbox.org>
2021-10-10 23:01:04 +0200

    fix updateAppVersionAndBuildDate.sh

--- updateAppVersionAndBuildDate.sh
+++ updateAppVersionAndBuildDate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s/.*var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main.go
-sed -i "s/.*var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main.go
+sed -i "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main.go
+sed -i "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main.go
