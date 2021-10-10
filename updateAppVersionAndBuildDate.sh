2f7f0887423a4505773f3143c97974d53cfee9bf
Amaury Bodet <amaury@mailbox.org>
2021-10-10 23:08:07 +0200

    fix updateAppVersionAndBuildDate.sh for MacOS

--- updateAppVersionAndBuildDate.sh
+++ updateAppVersionAndBuildDate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sed -i "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main.go
-sed -i "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main.go
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sed -i '' "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main.go
+	sed -i '' "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main.go
+else
+	sed -i "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main.go
+	sed -i "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main.go
+fi
