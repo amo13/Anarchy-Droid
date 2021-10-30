b84f879da28242ce8468b90c7abb5f1f5888b8ec
Amaury Bodet <amaury@mailbox.org>
2021-10-30 21:57:48 +0200

    fix retrieval of ResurrectionRemix download link

--- get/resurrectionremix.go
+++ get/resurrectionremix.go
@@ -113,7 +113,7 @@ func ResurrectionRemixLatestAvailableHref(codename string) (string, error) {
 		}
 	}
 
-	if dl_url == "" || !strings.HasSuffix(dl_url, ".zip") {
+	if dl_url == "" || (!strings.HasSuffix(dl_url, ".zip") && !strings.Contains(dl_url, ".zip?")) {
 		logger.Log("Unable to follow ResurrectionRemix sourceforge redirects to mirror")
 		return "", fmt.Errorf("unable to follow sourceforge redirects to mirror")
 	}
