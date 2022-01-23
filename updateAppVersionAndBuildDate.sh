7cb3f2f3750d9b82ef70ab5035041771ce8e08b1
Amaury Bodet <amaury@mailbox.org>
2022-01-24 00:05:54 +0100

    adapt to platform-specific main_.go files

--- updateAppVersionAndBuildDate.sh
+++ updateAppVersionAndBuildDate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i '' "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main.go
-	sed -i '' "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main.go
+	sed -i '' "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main_unix.go
+	sed -i '' "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main_unix.go
 else
-	sed -i "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main.go
-	sed -i "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main.go
+	sed -i "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main_unix.go
+	sed -i "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main_unix.go
+	sed -i "s/var AppVersion string.*/var AppVersion string = \"`awk -F'[ ="]+' '$1 == "Version" { print $2 }' FyneApp.toml`\"/" main_windows.go
+	sed -i "s/var BuildDate string.*/var BuildDate string = \"`date +%Y-%m-%d`\"/" main_windows.go
 fi
